17544454ca74fbdfced1eeb4482d7449a3d9acc0
KDONJS <yorlinssj@hotmail.com>
2024-01-06 23:43:40 -0500

    commit

---
+++ devops/jenkins_file_qa.groovy
@@ -0,0 +1,27 @@
+@Library('shared-library@develop')
+
+import sharedLibrary.toolsJenkisn
+
+def tools = new toolsJenkisn(steps, this)
+def nombre = "Yorlin"
+def credentialsId = "2"
+def remoteHost = "161.132.42.130"
+
+try {
+    node {
+        stage('Saludo de pipeline') {
+            tools.call("${nombre}" as String)
+        }
+        stage('Hacer un echo') {
+            script {
+                sh "ls"
+            }
+        }
+        stage('SonarCloud') {
+            tools.analisisCodigo()
+        }
+    }
+} catch (e) {
+    tools.notifySlack("Error: ${e}")
+    throw e
+}
